10c7b4c0891e6a911f02627420f0f83a03eaf3f6
Abebe Asnakew <abebeasnakew38@gmail.com>
2025-10-20 23:41:54 +0300

    mile to kilometer conversion functionality
    Implement mile to kilometer conversion with input validation.

--- Problem_2_Unit_Converter/solution.py
+++ Problem_2_Unit_Converter/solution.py
@@ -1,1 +1,7 @@
-# your code here
+#change mile to kilometer , where 1mile=1.61km
+mile=int(input("Enter the mile: "))
+if mile>=0:    
+    kilometer=mile*1.61    
+    print(f"The conversion of",mile,f"mile is: ",kilometer)
+else:    
+    print("Mile can not measure in Negative.")
