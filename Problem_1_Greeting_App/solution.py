397f7aa9696a5feed2503461c30614161fd1affb
Abebe Asnakew <abebeasnakew38@gmail.com>
2025-10-20 23:38:11 +0300

    Implement greeting app functionality

--- Problem_1_Greeting_App/solution.py
+++ Problem_1_Greeting_App/solution.py
@@ -1,1 +1,6 @@
-# your code here
+current_year=2018
+name=input("Enter your full name: ")
+favorite_food=input("Enter your favorite food: ")
+age=int(input("Enter your age: "))
+birth_year=current_year-age
+print ("Hello{name}.Your favorite food is{favorite_food}.Your birth year is{birth_year}.")
