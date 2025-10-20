c7c37032053d34d40551085959c14feeed234b68
Abebe Asnakew <abebeasnakew38@gmail.com>
2025-10-20 23:49:55 +0300

    simple calculator functionality
    Implemented a simple calculator that performs addition, subtraction, multiplication, and division. Added input handling for two numbers and an operator.

--- Problem_3_Simple_Calculator/solution.py
+++ Problem_3_Simple_Calculator/solution.py
@@ -1,1 +1,19 @@
-# your code here
+#simple calculator
+num1=int(input("Enter the first number:"))
+opp=input("oprator: ")
+num2=int(input("Enter the second number: "))
+if opp=='+':
+    result=num1+num2
+    print(f"The sum of the numbers are:",result)
+elif opp=='-':
+    result=num1-num2
+    print(f"The diverence of the numbers are:",result)
+elif opp=='/':
+    if num2!=0:
+        result=num1/num2
+        print(f"The diviion of the numbers are:",result)
+    elif num2 ==0: #We can not divided by 0. 
+        print("Any number divisible by 0 is imposible.")
+elif opp=='*':  
+    result=num1*num2
+    print(f"The multiplication of the numbers are:",result)
