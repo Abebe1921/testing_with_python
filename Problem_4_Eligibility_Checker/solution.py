89d6498578783d431b048d5fb83ff7faba860529
Abebe Asnakew <abebeasnakew38@gmail.com>
2025-10-20 23:54:51 +0300

    eligibility checker for car rental service

--- Problem_4_Eligibility_Checker/solution.py
+++ Problem_4_Eligibility_Checker/solution.py
@@ -1,1 +1,10 @@
-# your code here
+#Eligiblity checker for car rental service.
+age=int(input("Enter your age: "))
+if age>=21:
+    license=input("Do you have a license? ") 
+    if license == "yes": #all letters are must be small letter.
+        print("You are Eligiable to car rental.") 
+    else:
+        print("You are not eligiable for car rental")
+else:
+    print("You are not Eligiable for car rental.")
