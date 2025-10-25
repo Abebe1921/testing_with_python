19bc9dbbc94920b25cfccce058d7382ccdfff2c6
Abebe Asnakew <abebeasnakew38@gmail.com>
2025-10-26 04:00:27 +0900

    bitwise shift visualizer

--- worksheets/Bitwise Shift Visualizer.py
+++ worksheets/Bitwise Shift Visualizer.py
@@ -5,3 +5,27 @@ Show results of left shift and right shift.
 
 Print the binary form before and after.
 """
+
+num = int(input("Enter an integer: "))
+shift = int(input("Enter number of shift positions: "))
+
+binary_before = bin(num)
+
+left_shift = num << shift
+right_shift = num >> shift
+
+binary_left = bin(left_shift)
+binary_right = bin(right_shift)
+
+print(f"Original number: ",num)
+print(f"Binary (before): ",binary_before)
+
+print("Bitwise Shift Results")
+
+print(f"Left Shift by ",shift,":")
+print(f"Result: ",left_shift)
+print(f"Binary: ",binary_left)
+
+print(f"Right Shift by: ",shift)
+print(f"Result: ",right_shift)
+print(f"Binary: ",binary_right)
